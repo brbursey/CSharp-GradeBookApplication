b13561f16b1f751e211ad2a3624f1eb93c660bc0
bbursey <bbursey@emoneyadvisor.com>
2018-09-19 14:34:43 -0400

    Fixed some lines

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,9 +12,8 @@ namespace GradeBook.GradeBooks
         }
         public override char GetLetterGrade(double averageGrade)
         {
-
             if (Students.Count < 5)
-                throw new InvalidOperationException();
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
 
             var threshold = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
@@ -27,7 +26,7 @@ namespace GradeBook.GradeBooks
                 return 'C';
             else if (grades[threshold * 4] - 1 <= averageGrade)
                 return 'D';
-            else 
+            else
                 return 'F';
         }
     }
